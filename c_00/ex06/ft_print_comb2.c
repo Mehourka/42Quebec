1bf45a8848cd6260868f9884cfdc2ec573b10fed
Karim <k.mehour@gmail.com>
2022-11-27 06:11:44 -0500

    Fix ex06

--- c_00/ex06/ft_print_comb2.c
+++ c_00/ex06/ft_print_comb2.c
@@ -1,41 +1,41 @@
 #include <unistd.h>
-#include <stdio.h>
 
-void format(int n);
-void ft_print_comb2(void);
+void	format_2digits(int n);
 
-int main(void)
+void	ft_print_comb2(void)
 {
-    ft_print_comb2();
-    // format(22);
+	int	i;
+	int	j;
+	int	max = 99;
+
+	i = 0;
+	while (i <= max)
+	{
+		j = i + 1;
+		while (j <= max)
+		{
+			format_2digits(i);
+			write (1, " ", 1);
+			format_2digits(j);
+			if (i < (max - 1))
+				write(1, ", ", 2);
+			j++;
+		}
+		i++;
+	}
 }
 
-void ft_print_comb2(void)
+void	format_2digits(int n)
 {
-    for (int i = 0; i <= 3; i++)
-    {
-        if (i != 0)
-        {
-            write(1, ", ", 2);
-        }
-        format(i);
-    }
-}
+	char	str[2];
+	int		i;
 
-void format(int n)
-{
-    char str[5];
-    for (int i = 4; i >= 0; i--)
-    {
-        if (i == 2)
-        {
-            str[i] = ' ';
-        }
-        else
-        {
-            str[i] = n % 10 + '0';
-            n /= 10;
-        }
-    }
-    write(1, str, 5);
+	i = 1;
+	while (i >= 0)
+	{
+		str[i] = n % 10 + '0';
+		n /= 10;
+		i--;
+	}
+	write(1, str, 2);
 }

26eb51fc38ee177544ad793e49a4096c5872678d
Karim <k.mehour@gmail.com>
2022-11-28 20:03:04 -0500

    Add ex10

---
+++ 02-c/ex10/ft_strlcpy.c
@@ -0,0 +1,24 @@
+unsigned int	str_len(char *str);
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (str_len(src));
+}
+
+unsigned int	str_len(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}

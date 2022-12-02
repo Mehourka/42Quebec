e7fa2e176845847e70096f81d572aa9227618d19
Karim <k.mehour@gmail.com>
2022-12-02 09:04:44 -0500

    completed c04 and started c05

---
+++ 05-c/ex03/ft_recursive_power.c
@@ -0,0 +1,19 @@
+int ft_recursive_power(int nb, int power)
+{
+	if (power == 0)
+		return 1;
+
+	if (power < 0)
+		return 0;
+
+	if (nb == 0)
+		return 0;
+
+	return (nb * ft_recursive_power(nb, power - 1));
+}
+
+#include <stdio.h>
+int main(void)
+{
+	printf("%i\n", ft_recursive_power(5, 1));
+}
